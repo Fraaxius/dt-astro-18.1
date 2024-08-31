b48e3a8cdc3afaf28bf7c2f3ae701a1af9c9c737
Erfan Abdi <erfangplus@gmail.com>
2024-08-31 12:16:11 -0400

    astro: Set ro.vendor.fm.use_audio_session

--- properties.mk
+++ properties.mk
@@ -6,6 +6,10 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.physical.num=4
 
+# FM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.fm.use_audio_session=true
+
 # Sensors
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.vendor.sensors.mot_ltv=true
