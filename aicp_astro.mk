cc9b7fbf2663577fb48d90bf02aa847e4e988267
Fraaxius <franthekillaz@gmail.com>
2024-09-07 23:02:50 -0400

    astro: AICP-ify

--- aicp_astro.mk
+++ aicp_astro.mk
@@ -22,10 +22,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/astro/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_astro
+PRODUCT_NAME := aicp_astro
 PRODUCT_DEVICE := astro
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := Motorola One Fusion
